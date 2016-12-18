4714a237a90919ac680cbc6870a7e7c168be2dc2
Andrew Kim 18E <akim17@rstudio.amherst.edu>
2016-12-18 01:07:54 -0500

    changes

--- R/text_prep.R
+++ R/text_prep.R
@@ -18,7 +18,7 @@
 get_breaks <- function(text, breaks){
   output <- c()
   for(i in 1:(length(breaks) + 1)){
-    output <- c(output, grep(breakpoints[i], text))
+    output <- c(output, which(gardenParty.text == breakpoints[i]))
   }
   return(output)
 }
